4018d8a79f31f5277b72117483391dba1f3e9ca1
YutaGoto <you.goto.510@gmail.com>
2020-11-07 13:37:33 +0900

    init commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
+import React, { FC } from "react";
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home: FC = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -63,3 +64,6 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
+
